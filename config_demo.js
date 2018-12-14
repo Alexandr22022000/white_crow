73addaadb127c14657b5109974d612f75843decc
Alex <zol22222@mail.ru>
2018-12-15 04:49:11 +0700

    Add prod database

--- config_demo.js
+++ config_demo.js
@@ -1,5 +1,5 @@
 module.exports = {
-    DATABASE_URL: 'postgres://postgres:0000%@localhost:5432/white_crow',
+    DATABASE_URL: 'postgres://ktxjcyakmvmben:255bbf9724211026b6ad14857bd338e3c3395626943f8d91422b20098258c42a@ec2-54-243-238-46.compute-1.amazonaws.com:5432/d519u7oig4i8n8?ssl=true',
     TOKEN_SECRET: 'This is my secret!',
     TOKEN_DATA: 'This is data!',
     UID: 'eb61e19f-d2a2-466e-a2b5-f759258a1166',
